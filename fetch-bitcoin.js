0493eafddb47a36220d67710f417cecee90d560e
luwanhika <luwanhika@hotmail.com>
2021-05-26 20:46:53 -0300

    requisiçãp api

--- fetch-bitcoin.js
+++
@@ -1,5 +0,0 @@
-export default function initFetchBitcoin() {
-
-}
-
-// https://www.blockchain.com/ticker
---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,11 @@
+export default function initFetchBitcoin() {
+  fetch('//blockchain.com/ticker')
+  .then(response => response.json())
+  .then(bitcoin => {
+    const btcPreco = document.querySelector('.btc-preco');
+    btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+  }).catch(erro => {
+    console.log(Error(erro));
+  })
+}
+
